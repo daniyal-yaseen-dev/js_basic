89d242d59cd4f1bae866e47577b85318a25d7e1d
daniyal-yaseen-dev <daniyalyaseen67@gmail.com>
2025-12-24 05:40:06 +0000

    Conersion of datatypes

---
+++ baisc/conversion.js
@@ -0,0 +1,8 @@
+console.log("Hello world");
+let score = "74aa";
+console.log(typeof score );
+console.log(typeof (score) );
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log( valueInNumber);
+// 
